8eb1c525c8965455f82a8f04d32b7580c581a1bb
Susanna Pezzini <susanna.pezzini@gmail.com>
2021-02-18 17:48:25 +0000

    add all working ahahhahah

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :cocktails, only: %i[index show new create]
   root to: 'cocktails#index'
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create destroy]
+  end
 end

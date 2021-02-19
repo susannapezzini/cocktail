37a5eaf55563ef798e33d76bc79c65d9b12812e9
Susanna Pezzini <susanna.pezzini@gmail.com>
2021-02-19 15:20:56 +0000

    img allllllright

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,11 +1,18 @@
 module CocktailsHelper
-  
+
   def cocktail_image_path(cocktail)
     if cocktail.photo.attached?
-      cl_image_path(cocktail.photo.key)
       cl_image_tag(cocktail.photo.key)
     else
       # cl_image_path('cocktail_placeholder')
+      image_tag 'bg.jpg', class: 'img'
+    end
+  end
+
+  def cocktail_asset_path(cocktail)
+    if cocktail.photo.attached?
+      cl_image_path(cocktail.photo.key)
+    else
       asset_path('bg.jpg')
     end
   end

72be4b544e05bcbdfc85fff4562af74258f94d08
Susanna Pezzini <susanna.pezzini@gmail.com>
2021-02-19 15:48:38 +0000

    let's nìbe hopful

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,11 +1,12 @@
 module CocktailsHelper
 
-  def cocktail_image_path(cocktail)
+  def cocktail_image_path(cocktail, options = {})
     if cocktail.photo.attached?
       cl_image_tag(cocktail.photo.key)
     else
       # cl_image_path('cocktail_placeholder')
-      image_tag 'bg.jpg'
+      image_tag 'bg.jpg', options
+    end
   end
 
   def cocktail_asset_path(cocktail)
